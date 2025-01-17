febb3b576e88fb9344f2e0245c03dc28a8b30b1f
ROHIT9991 <142079391+ROHIT9991@users.noreply.github.com>
2025-01-17 20:17:41 +0530

    Delete module3 directory

--- module3/src/main/java/com/infosys/module3/exception/Module3Exception.java
+++
@@ -1,8 +0,0 @@
-package com.infosys.module3.exception;
-
-public class Module3Exception extends Exception{
-    public Module3Exception(String message)
-    {
-        super(message);
-    }
-}
--- module3/src/main/java/com/infosys/module3/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.infosys.module3.repository;
-
-import com.infosys.module3.model.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event,Long> {
-}
